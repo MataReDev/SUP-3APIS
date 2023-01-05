e858509140b9cddde637072a44e03a031fb7b37f
MataRed-dev <matared-dev@github.com>
2023-01-05 19:06:21 +0100

    Add comments and gitignore

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
 
+/* ROUTES */
 const userRoutes = require("./src/routes/UserRoute");
 const trainRoutes = require("./src/routes/TrainRoutes");
 const trainStationRoutes = require("./src/routes/TrainStationRoutes");
@@ -22,11 +23,12 @@ const app = express();
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+/* Gestion des routes pour l'uri */
 app.use("/user",userRoutes);
 app.use("/train",trainRoutes);
 app.use("/trainstation",trainStationRoutes);
 
+/* AFFICHAGE DU PORT SUR LEQUEL */
 app.listen(PORT, () => {
     console.log(`App listening on ${PORT}`);
 })
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,20 +1,27 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+  // Email unique en base
     email: {
       type: String,
       required: true,
       unique: true
     },
+    
+  // Pseudo unique en base
     pseudo: {
       type: String,
       required: true,
       unique: true
     },
+    
+  // Mot de passe (utilisation de bcrypt pour hash le mdp)
     password: {
       type: String,
       required: true
     },
+    
+  // role uniquement dans la liste décrite en dessous avec comme valeur de base User
     role: {
       type: String,
       required: true,
